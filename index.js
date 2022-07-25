34f5645266919d79ad701ca633df83c19b447679
Juan Pino <juanrespolo@gmail.com>
2022-07-25 14:19:14 -0500

    delete fav

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const DBConnection = require('./DBConnection')
 const app = express();
 
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user")
 
 app.use(express.json())
 app.use(cors())
 app.use("/api/auth",authRoute)
+app.use("/api/user",userRoute)
 
 
 app.listen({port: process.env.PORT || 5000},async()=>{
--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,64 @@
 const router = require("express").Router();
-const {
-    verifyToken
-} = require("../middlewares/verifyToken");
-const User = require("../models/user")
+const { verifyToken } = require("../middlewares/verifyToken");
+const User = require("../models/user");
 
-router.post("/fav", verifyToken, function (req, res) {
-    const { id } = req.body
+router.post("/fav", verifyToken, async function (req, res) {
+  const { id } = req.body;
 
-    if (!id) {
-        return res.status(406).send({
-            ok: false,
-            error: "there is no id of fav"
-        })
-    }
-    try {
-        const userUpdated = await User.updateOne(
-            { _id: req.user.id },
-            {
-                $addToSet: {
-                    favs: { id: id },
-                },
-            },
-            {
-                new: true 
-            }
-        );
-        return res.status(200).send(userUpdated)
-    } catch (e) {
-        return res.status(500).send({
-            ok: false,
-            error: e
-        })
-    }
-})
+  if (!id) {
+    return res.status(406).send({
+      ok: false,
+      error: "there is no id of fav",
+    });
+  }
+  try {
+    const userUpdated = await User.findOneAndUpdate(
+      { _id: req.user.id },
+      {
+        $addToSet: {
+          favs: { id: id },
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    return res.status(200).send(userUpdated);
+  } catch (e) {
+    return res.status(500).send({
+      ok: false,
+      error: e,
+    });
+  }
+});
+
+router.delete("/fav", verifyToken, async function (req, res) {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(406).send({
+      ok: false,
+      error: "there is no id of fav",
+    });
+  }
+
+  try {
+    const userUpdated = await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        $pull: {
+          favs: { id: id },
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).send(userUpdated);
+  } catch (e) {
+    return res.status(500).send({
+      ok: false,
+      error: e,
+    });
+  }
+});
+
+module.exports = router;
